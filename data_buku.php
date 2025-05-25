e1d98dffb173111e3e45f6790f9023688766da41
PratamaAngga <anggashillnoir@gmail.com>
2025-05-25 20:53:19 +0700

    tampilin data buku

--- data_buku.php
+++ data_buku.php
@@ -78,6 +78,11 @@ if (isset($_POST['simpan'])) {
         .mx-auto {width: 800px;}
         .card {margin-top: 10px;}
     </style>
+    
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.3.1/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.3.1/js/dataTables.bootstrap5.js"></script>
 </head>
 <body id="page-top">
     <!-- Page Wrapper -->
@@ -86,7 +91,7 @@ if (isset($_POST['simpan'])) {
         <ul class="navbar-nav bg-admin sidebar sidebar-light accordion" id="accordionSidebar">
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/admin/dashboard">
-                <img src="../img/logo aplikasi billa 1.png" />
+                <img src="image/Readify..png" />
             </a>
 
             <!-- Nav Item - Dashboard -->
@@ -104,6 +109,192 @@ if (isset($_POST['simpan'])) {
             </li>
         </ul>
         <!-- End of Sidebar -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Buku</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img src="" class="img-fluid" id="gambar-buku">
+                                </div>
+                                <div class="col-md-8">
+                                    <ul class="list-group">
+                                        <li class="list-group-item mb-4">
+                                            <h3 id="judul-buku"></h3>
+                                        </li>
+                                        <label for="kategori-buku">Kategori :</label>
+                                        <li class="list-group-item mb-4" id="kategori-buku"></li>
+                                        <label for="kategori-buku">Penulis :</label>
+                                        <li class="list-group-item mb-4" id="penulis-buku"></li>
+                                        <label for="kategori-buku">Penerbit :</label>
+                                        <li class="list-group-item mb-4" id="penerbit-buku"></li>
+                                        <label for="kategori-buku">Tahun Terbit :</label>
+                                        <li class="list-group-item mb-4" id="terbit-buku"></li>
+                                        <label for="kategori-buku">Stok :</label>
+                                        <li class="list-group-item mb-4" id="stok-buku"></li>
+                                        <label for="kategori-buku">Deskripsi :</label>
+                                        <li class="list-group-item mb-4" id="deskripsi-buku"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <div class="header-navbar">
+                        <h2>Data Buku</h2>
+                        <p>Data Buku</p>
+                    </div>
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Nav Item - Alerts -->
+                        <li class="nav-item dropdown no-arrow mx-1">
+                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-bell fa-fw"></i>
+                                <!-- Counter - Alerts -->
+                                <span class="badge badge-danger badge-counter">!</span>
+                            </a>
+                            <!-- Dropdown - Alerts -->
+                            <div
+                                class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="alertsDropdown" id="notifAdmin"
+                                >
+                                <h6 class="dropdown-header">Notifikasi</h6>
+                            </div>
+                        </li>
+
+                        <div class="topbar-divider d-none d-sm-block"></div>
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img class="mr-3 img-profile rounded-circle" src="../img/undraw_profile.svg" />
+                                <span class="d-none d-lg-inline text-gray-600 small" id="user"> (Petugas)</span>
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Profile
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" style="cursor: pointer;" onclick="logout()">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Keluar
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <!-- Button Print Laporan -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <a href="/admin/Tambah-Buku"><button class="btn-tambah">
+                                <i class="bi bi-plus-lg"></i>Tambah Buku
+                            </button></a>
+                    </div>
+
+                    <!-- Content Row -->
+                    <!-- Project Card Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-orange">
+                                List Data Buku
+                            </h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover" id="tabelBuku">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Judul</th>
+                                            <th scope="col">Pengarang</th>
+                                            <th scope="col">Penerbit</th>
+                                            <th scope="col">Tahun Terbit</th>
+                                            <th scope="col">Stok</th>
+                                            <th scope="col">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $sql2 = "select * from buku order by id_buku";
+                                        $q2   = mysqli_query($koneksi,$sql2);
+                                        $urut = 1;
+                                        while ($r2 = mysqli_fetch_array($q2)) {
+                                            $id_buku = $r2['id_buku'];
+                                            $judul = $r2['judul'];
+                                            $pengarang = $r2['pengarang'];
+                                            $penerbit = $r2['penerbit'];
+                                            $tahun_terbit = $r2['tahun_terbit'];
+                                            $stok = $r2['stok'];
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $urut++ ?></td>
+                                            <td><?php echo $judul ?></td>
+                                            <td><?php echo $pengarang ?></td>
+                                            <td><?php echo $penerbit ?></td>
+                                            <td><?php echo $tahun_terbit ?></td>
+                                            <td><?php echo $stok ?></td>
+                                            <td>
+                                                <a href="index.php?op=edit&id=<?php echo $id_buku ?>"><button type="button" class="btn btn-warning btn-sm">Edit</button></a>
+                                                <a href="index.php?op=delete&id=<?php echo $id_buku ?>" onclick="return confirm('Yakin mau delete data?')"><button type="button" class="btn btn-danger btn-sm">Delete</button></a>
+                                            </td>
+                                        </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2021</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+        </div>
+    </div>   
     <div class="mx-auto">
     <!-- // untuk masukkan data -->
         <div class="card">
@@ -168,5 +359,11 @@ if (isset($_POST['simpan'])) {
                 </form>
             </div>
         </div>
+    </div>
+    <script>
+        $(document).ready(function () {
+            $('#tabelBuku').DataTable();
+        });
+    </script>
 </body>
 </html>
